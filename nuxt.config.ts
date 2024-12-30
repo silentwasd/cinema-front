ea7c65ac79066a75f078d3435fb125effa83d887
Ilya Ignatev <silentwasd@gmail.com>
2024-12-30 12:16:22 +0300

    Mobile viewport

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -30,5 +30,13 @@ export default defineNuxtConfig({
                 ital: [300]
             }
         }
+    },
+
+    app: {
+        head: {
+            meta: [
+                {name: 'viewport', content: 'width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no'}
+            ]
+        }
     }
 })
