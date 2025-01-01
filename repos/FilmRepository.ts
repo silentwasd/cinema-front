5a4f73ac9d8ebc4c554122a2cffdad17ab396b91
Ilya Ignatev <silentwasd@gmail.com>
2025-01-01 22:31:03 +0300

    Post put files

--- repos/FilmRepository.ts
+++ repos/FilmRepository.ts
@@ -2,5 +2,7 @@ import type Film from "~/resources/Film";
 import CrudRepository from "~/repos/CrudRepository";
 
 export default class FilmRepository extends CrudRepository<Film, number> {
-    protected baseUrl: string = '/management/films';
+    protected baseUrl: string             = '/management/films';
+    protected override postFiles: boolean = true;
+    protected override putFiles: boolean  = true;
 }
