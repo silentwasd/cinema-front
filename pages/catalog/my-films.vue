9b1f4dcd6be6c420b0301e34c776ddd7ff86d2d8
Ilya Ignatev <silentwasd@gmail.com>
2025-01-11 23:36:50 +0300

    Fix link in my films

--- pages/catalog/my-films.vue
+++ pages/catalog/my-films.vue
@@ -102,7 +102,7 @@ async function remove(watcher: FilmWatcher) {
 
             <template #film.name-data="{row}">
                 <NuxtLink class="flex items-center gap-2.5 hover:underline underline-offset-2"
-                          :to="`/management/films/${row.film.id}`">
+                          :to="`/catalog/films/${row.film.id}`">
                     <div v-if="row.film.cover"
                          class="bg-no-repeat bg-cover bg-center rounded w-8 h-8"
                          :style="`background-image: url(${fileUrl(row.film.cover)})`"></div>
